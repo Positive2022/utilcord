1608d6e8f69a5f00fe09e538f85c45bc1a76bb4f
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-08-06 11:38:44 -0400

    Create client

---
+++ struct/client.js
@@ -0,0 +1,40 @@
+const { Client, Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+class UtilsClient extends Client {
+    constructor(options) {
+        super(options);
+
+        this.commands = new Collection();
+    }
+
+    loadCommands(commandsDir) {
+        const commands = require('require-all')(commandsDir);
+
+        for (const [, command] of commands) {
+            client.commands.set(command);
+        }
+    }
+
+    async deployCommands(token, clientId, guildId) {
+        const rest = new REST({ version: '9' }).setToken(token);
+
+        try {
+            if (guildId) {
+                await rest.put(
+                    Routes.applicationGuildCommands(clientId, guildId),
+                    { body: client.commands.map(command => command.data) }
+                );
+            } else {
+                await rest.put(Routes.applicationCommands(clientId), {
+                    body: client.commands.map(command => command.data)
+                });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+module.exports = UtilsClient;
