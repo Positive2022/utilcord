7113baca09a6f299e7a7ce7b990fdc497c8a4349
positive2022 <manikagarwal3@gmail.com>
2021-08-07 23:05:29 +0530

    Tickets but not tested

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
   Fun: require('./modules/fun/fun'),
   Player: require('./modules/player/index'),
   Others: require('./modules/others/index'),
+  Ticket: require('./modules/tickets/ticket'),
   UtilsClient: require('./struct/client')
 };
