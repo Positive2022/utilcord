9eede69134b65dbe63a5173a28f6a1e0c8042329
positive2022 <manikagarwal3@gmail.com>
2021-08-07 20:34:07 +0530

    fix file name

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
     UtilsClient: require('./struct/client'),
     Command: require('./struct/command'),
     Player: require('./modules/player/index'),
-    Fun: require('./modules/fun/index'),
+    Fun: require('./modules/fun/fun'),
   Animals: require('./modules/animals/animal')
 };
