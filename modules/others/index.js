13e7d9e8359419b1844e356cf579eabc1ad47e9c
positive2022 <manikagarwal3@gmail.com>
2021-08-07 22:08:31 +0530

    edit index of others

--- modules/others/index.js
+++ modules/others/index.js
@@ -1,1 +1,9 @@
-module.exports = require('./functions/anime')
+const fs = require("fs");
+module.exports = async () => {
+
+    const files = fs.readdirSync("./functions/");
+    for (const file of files) {
+        const functions = require("require-all")(__dirname + `/functions/${file}`);
+        Object.assign(module.exports, functions);
+    }
+}
