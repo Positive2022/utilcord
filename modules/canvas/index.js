57c6a1ad5963eb5f660e5a5939b35fd73def097a
positive2022 <manikagarwal3@gmail.com>
2021-08-08 14:11:58 +0530

    awooify function

--- modules/canvas/index.js
+++ modules/canvas/index.js
@@ -2,9 +2,24 @@ class Canvas {
 
     async avatar(interaction, userID, options = {}) {
         if (!userID || typeof userID !== 'string') userID = interaction.user.id
-            let user = await message.client.users.fetch(userID);
+        let user = await message.client.users.fetch(userID);
         if (!user) user = interaction.user
         return user.displayAvatarURL(options)
+    }
+
+    async awooify(interaction, userID, options = {}) {
+        const av = this.avatar(interaction, userID, options = {
+            format: "png",
+            dynamic: false
+        })
+        try {
+            var res = await fetch(`https://nekobot.xyz/api/imagegen?type=awooify&url=${encodeURIComponent(av)}`);
+            var json = await res.json();
+            if (!json.message) throw new Error(`Not Found.`);
+        } catch (err) {
+           throw new Error(err)
+        }
+        return json.message
     }
 }
 
