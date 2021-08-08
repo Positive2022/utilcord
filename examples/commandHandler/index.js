8c7ea60c243ac66e22c21ddd86925f186d39fb27
positive2022 <manikagarwal3@gmail.com>
2021-08-08 12:07:39 +0530

    update examples

--- examples/commandHandler/index.js
+++ examples/commandHandler/index.js
@@ -1,9 +1,38 @@
-const { token } = require("./config.json");
-const { Intents } = require('discord.js');
+const  token = "get token somehow"
+const Discord = require('discord.js')
+var intent = new Discord.Intents(Object.values(Discord.Intents.FLAGS))
+intent.remove(['GUILD_PRESENCES', 'DIRECT_MESSAGE_REACTIONS', 'DIRECT_MESSAGE_TYPING', 'GUILD_MESSAGE_TYPING']) // Your intents.
 
 const { UtilsClient } = require('utilcord');
 const client = new UtilsClient({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+    intents: intent,
+    //Enable cache by commenting them out.
+        makeCache: Discord.Options.cacheWithLimits({
+        //ApplicationCommandManager: 0,
+        //BaseGuildEmojiManager: 0,
+        //ChannelManager: 0,
+        //GuildBanManager: 0,
+        //GuildChannelManager: 0,
+        //GuildInviteManager: 0,
+        //GuildManager: 0,
+        GuildMemberManager: 0,
+        //GuildStickerManager: 0,
+        //MessageManager: 0,
+        //PermissionOverwriteManager: 0,
+        PresenceManager: 0,
+        //ReactionManager: 0,
+        ReactionUserManager: 0,
+        //RoleManager: 0,
+        //StageInstanceManager: 0,
+        //ThreadManager: 0,
+        //ThreadMemberManager: 0,
+        UserManager: 0,
+        //VoiceStateManager: 0
+    }),
+    allowedMentions: {
+        parse: ['roles'],
+        repliedUser: true
+    }
 });
 
 client.on('ready', async () => {
