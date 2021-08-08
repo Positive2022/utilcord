0a6fedc85ddb91dfa75021bcac162d64b537a486
positive2022 <manikagarwal3@gmail.com>
2021-08-08 12:22:54 +0530

    remove music causing errors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
   Animals: require('./modules/animals/animal'),
   Command: require('./struct/command'),
   Fun: require('./modules/fun/fun'),
-  Player: require('./modules/player/index'),
   Others: require('./modules/others/index'),
   Ticket: require('./modules/tickets/ticket'),
   UtilsClient: require('./struct/client')
